d42763ea52d7ca664cfdd4945a06b1ea47b16d4e
Jeff Voss <jefjones_Macbook@Jeffs-MacBook.local>
2019-06-04 12:14:34 -0700

    build out event stream

--- src/app/modules/rest/components/rest/rest.component.ts
+++ src/app/modules/rest/components/rest/rest.component.ts
@@ -18,50 +18,40 @@ export class RestComponent implements OnInit {
   }
 
   data: any = {};
+  eventStream: any[] = [
+    { url: 'https://jsonplaceholder.typicode.com/todos/1', channel: 'login', subject: 'login.event', title: 'Event received from Channel A', delay: 1000 },
+    { url: 'https://jsonplaceholder.typicode.com/todos/1', channel: 'dashboard', subject: 'dashboard.event', title: 'Event received from Channel B', delay: 2000 },
+    { url: 'https://jsonplaceholder.typicode.com/todos/1', channel: 'login', subject: 'last.login', title: 'Other Event received from Channel A', delay: 3000 },
+    { url: 'https://jsonplaceholder.typicode.com/todos/1', channel: 'login', subject: 'login.event', title: 'Event received from Channel A', delay: 4000 },
+    { url: 'https://jsonplaceholder.typicode.com/todos/1', channel: 'dashboard', subject: 'dashboard.event', title: 'Event received from Channel B', delay: 5000 },
+    { url: 'https://jsonplaceholder.typicode.com/todos/1', channel: 'login', subject: 'login.event', title: 'Other Event received from Channel A', delay: 6000 },
+    { url: 'https://jsonplaceholder.typicode.com/todos/1', channel: 'login', subject: 'login.event', title: 'Event received from Channel A', delay: 7000 },
+    { url: 'https://jsonplaceholder.typicode.com/todos/1', channel: 'dashboard', subject: 'dashboard.event', title: 'Event received from Channel B', delay: 8000 },
+    { url: 'https://jsonplaceholder.typicode.com/todos/1', channel: 'login', subject: 'last.login', title: 'Other Event received from Channel A', delay: 9000 },
+    { url: 'https://jsonplaceholder.typicode.com/todos/1', channel: 'login', subject: 'login.event', title: 'Event received from Channel A', delay: 10000 },
+    { url: 'https://jsonplaceholder.typicode.com/todos/1', channel: 'dashboard', subject: 'dashboard.event', title: 'Event received from Channel B', delay: 11000 },
+    { url: 'https://jsonplaceholder.typicode.com/todos/1', channel: 'login', subject: 'login.event', title: 'Other Event received from Channel A', delay: 12000 }
+  ]
+
+  eventMessages = {
+    login: 'Login Event Sent!',
+    dashboard: 'Dashboard Event Sent!'
+  }
 
   ngOnInit() {
-    this._restService.get('https://jsonplaceholder.typicode.com/todos/1')
-      .pipe(
-        delay(2000)
-      )
-      .subscribe(data => {
-        this._runEngineService.channel('login')
-          .subject('login.event')
-          .next({
-            title: 'Event received from Channel A'
-          });
+    this.eventStream.forEach(event => {
+      this._restService.get(event.url)
+        .pipe(
+          delay(event.delay)
+        )
+        .subscribe(data => {
+          this._runEngineService.channel(event.channel)
+            .subject(event.subject)
+            .next({ title: event.title });
 
-        this.data['events'].push('Login Event Sent!');
-      });
-
-    this._restService.get('https://jsonplaceholder.typicode.com/todos/2')
-      .pipe(
-        delay(1000)
-      )
-      .subscribe(data => {
-        this._runEngineService.channel('dashboard')
-          .subject('dashboard.event')
-          .next({
-            title: 'Event received from Channel B'
-          });
-
-        this.data['events'].push('Dashboard Event Sent!');
-      });
-
-    this._restService.get('https://jsonplaceholder.typicode.com/todos/1')
-      .pipe(
-        delay(3000)
-      )
-      .subscribe(data => {
-        this._runEngineService.channel('login')
-          .subject('last.login')
-          .next({
-            title: 'Other Event received from Channel A'
-          });
-
-        this.data['events'].push('Other Login Event Sent!');
-      });
-
+          this.data['events'].push(this.eventMessages[event.channel]);
+        });
+    });
   }
 
 }
